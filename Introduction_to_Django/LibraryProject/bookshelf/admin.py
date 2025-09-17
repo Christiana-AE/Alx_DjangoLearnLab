cd65af4ece1c9e294265b0e74b0eef9b2655c382
Christiana <christine.anako@gmail.com>
2025-09-17 14:38:43 +0100

    update

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -8,6 +8,6 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_date')
+    list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-    list_filter = ('publication_date',)
+    list_filter = ('publication_year',)
